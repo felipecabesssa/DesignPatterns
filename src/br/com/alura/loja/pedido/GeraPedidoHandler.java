7f15a73239607bd8686de1eeda0682acb9ec4202
felipecabesssa <felipesoares100@hotmail.com>
2022-01-09 01:32:57 -0300

    Aplicando o Pattern Observer

--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,25 +1,26 @@
 package br.com.alura.loja.pedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import br.com.alura.loja.Orcamento;
-import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
-import br.com.alura.loja.pedido.acao.SalvarPedidoNoBD;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
 
 public class GeraPedidoHandler {
 	
+	private List<AcaoAposGerarPedido> acoes;
+	
+	public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+		this.acoes = acoes;
+	}
+
 	public void execute(GeraPedido dados) {
 		
 		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
 		
 		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 		
-		EnviarEmailPedido email = new EnviarEmailPedido();
-		SalvarPedidoNoBD salvar = new SalvarPedidoNoBD();
-		
-		email.executar(pedido);
-		salvar.executar(pedido);
-		
+		acoes.forEach(a -> a.executarAcao(pedido));
 	}
 
 }
