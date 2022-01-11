3e022b4426474fdd9520320c7236727789a15514
felipecabesssa <felipesoares100@hotmail.com>
2022-01-11 13:11:08 -0300

    Aplicando o pattern Proxy-Cache

--- src/br/com/alura/loja/TestesComposicao.java
+++ src/br/com/alura/loja/TestesComposicao.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.orcamento.OrcamentoProxy;
 
 public class TestesComposicao {
 
@@ -17,8 +18,10 @@ public class TestesComposicao {
 		novo.adicionarItem(new ItemOrcamento(new BigDecimal("1500")));
 		novo.adicionarItem(antigo);
 		
-		System.out.println(novo.getValor());
-		System.out.println(novo.getValor());
+		OrcamentoProxy proxy = new OrcamentoProxy(novo);
+		
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
 	}
 
 }
